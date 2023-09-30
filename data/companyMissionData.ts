9197ea3e6628b311da83a8171e472a21fdea6161
danmiles <41701228+danmiles@users.noreply.github.com>
2023-09-30 19:00:14 +0200

    Section mission ready

---
+++ data/companyMissionData.ts
@@ -0,0 +1,23 @@
+type CompanyMissionType= {
+  title: string;
+  iconUrl: string;
+}[];
+
+export const companyMissionData: CompanyMissionType = [
+  {
+    title: 'Creating a bridge between Wall Street and the crypto industry',
+    iconUrl: '/images/home/company-mission/icons/bridge.svg',
+  },
+  {
+    title: 'Enabling global adaption of cryptocurrency',
+    iconUrl: '/images/home/company-mission/icons/cloud.svg',
+  },
+  {
+    title: 'Converting reluctant investors into blockchain experts',
+    iconUrl: '/images/home/company-mission/icons/people.svg',
+  },
+  {
+    title: 'Bringing the world`s first Bloomberg Terminal for crypto to the markets',
+    iconUrl: '/images/home/company-mission/icons/world.svg',
+  },
+];
--- sections/home/CompanyMission.tsx
+++ sections/home/CompanyMission.tsx
@@ -5,6 +5,9 @@ import Image from 'next/image';
 // Components
 import SectionHeader from '@/components/SectionHeader';
 
+// Data
+import { companyMissionData } from '@/data/companyMissionData';
+
 export default function CompanyMission() {
   return (
     <section className={`${styles.companyMission} home-sect-p`}>
@@ -13,27 +16,44 @@ export default function CompanyMission() {
         alt="mission-bg"
         className={styles.bgSectionImg}
         fill={true}
+        quality={100}
       />
       <div className="container">
         <div className={styles.header}>
           <div className={styles.headerImgBox}>
-          <Image
-            src="/images/home/company-mission/circle-crypto.png"
-            alt="Crypto currency"
-            width={324}
-            height={329}
-            className={styles.headerImg}
-          />
+            <Image
+              src="/images/home/company-mission/circle-crypto.png"
+              alt="Crypto currency"
+              width={324}
+              height={329}
+              className={styles.headerImg}
+            />
           </div>
 
-
           <SectionHeader
+            cssClassName={styles.headerTitle}
             subtitle="Company mission"
             titleBeforeAccent="Our mission is to help facilitate the next bull run
             for "
             titleAccent="cryptocurrency"
             description="Our company is building the world`s first cryptocurrency research platform. We`ve established world-class strategic partnerships – which provides us with unrivaled data libraries and best-in-class industry research. We stand to be a multi-billion dollar Bloomberg Terminal equivalent for cryptocurrency."
           />
+        </div>
+
+        <div className={styles.mission}>
+          {companyMissionData.map((item, index) => (
+            <div className={styles.missionItem} key={index}>
+              <Image
+                src={item.iconUrl}
+                alt="icon"
+                width={100}
+                height={100}
+                className={styles.missionIcon}
+              />
+
+              <div className={styles.missionItemTitle}>{item.title}</div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
