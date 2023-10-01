0a1c5508c98d2d69d3808689018da53b87c15933
danmiles <41701228+danmiles@users.noreply.github.com>
2023-10-02 01:15:17 +0200

    commit

---
+++ data/ourPriceData.ts
@@ -0,0 +1,25 @@
+type OurPiceType = {
+  title: string;
+}[]
+
+export const ourPriceData: OurPiceType = [
+  {
+    title: 'Our platform as a centralized source of cryptocurrency coverage',
+  },
+  {
+    title: 'Our former Bloomberg Terminal engineers',
+  },
+  {
+    title: 'Our industry low pricing',
+  },
+  {
+    title: 'Our platform`s institutionally tailored interface',
+  },
+  {
+    title: 'Our unrivaled coverage of the crypto market',
+  },
+  {
+    title: 'Our platform`s exclusive features',
+  },
+
+]
--- sections/home/OurPrice.tsx
+++ sections/home/OurPrice.tsx
@@ -5,6 +5,9 @@ import Image from 'next/image';
 // Components
 import SectionHeader from '@/components/SectionHeader';
 
+// Data
+import { ourPriceData } from '@/data/ourPriceData';
+
 export default function OurPrice() {
   return (
     <section className={`${styles.ourPrice} home-sect-p`}>
@@ -39,18 +42,59 @@ export default function OurPrice() {
               alt="price background"
               fill
             />
-            <h3 className={styles.headerContentTitle}>
-              Stocks, Bonds, and Derivatives 
-            </h3>
+            <div className={styles.headerContentLeft}>
+              <h3 className={styles.headerContentTitle}>
+                Stocks, Bonds, and Derivatives 
+              </h3>
+              <p>
+                Our hardware (blockchain keyboard + monitor hardware) is still
+                in the planning stage. 
+              </p>
+              <p>
+                Once we have completed prototyping, we will pursue a strategic
+                partnership with a peripherals manufacturer to facilitate a
+                highly-afford one-time price for the hardware.
+              </p>
+            </div>
+
+            <Image
+              className={styles.headerImgRight}
+              src="/images/home/our-price/svgman.svg"
+              alt="price background"
+              width={258}
+              height={236}
+            />
+          </div>
+        </div>
+        {/* header end */}
+
+        <div className={styles.contentBottom}>
+          <div className={styles.contentBottomLeft}>
+            <h4 className={styles.contentBottomTitle}>
+              Available to small investment companies and private investors
+            </h4>
             <p>
-              Our hardware (blockchain keyboard + monitor hardware) is still
-              in the planning stage. 
+              The annual licensing fee for the Satoshi Terminal will be $840 per
+              user compared to the Bloomberg Terminal’s annual licensing fee of
+              $27,000 per user.
             </p>
-            <p>
-              Once we have completed prototyping, we will pursue a strategic
-              partnership with a peripherals manufacturer to facilitate a
-              highly-afford one-time price for the hardware.
-            </p>
+            <div className={styles.contentBottomTabs}>
+              {ourPriceData.map((item, index) => (
+                <div key={index} className={styles.tabsItem}>
+                  {item.title}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={styles.contentBottomRight}>
+            <Image
+              className={styles.contentBottomImg}
+              src="/images/home/our-price/brand-asset.png"
+              alt="price background"
+              width={558}
+              height={495}
+              quality={90}
+            />
           </div>
         </div>
       </div>
