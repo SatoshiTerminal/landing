d8527adf252b216e93a93d56a31de363eabd4289
danmiles <41701228+danmiles@users.noreply.github.com>
2023-10-16 17:52:56 +0200

    Update send-email.ts

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -2,7 +2,8 @@
 import { WaitingListFormData } from '@/components/WaitingListForm';
 
 export function sendEmail(data: WaitingListFormData) {
-  const apiEndpoint = '/api/waiting-list';
+  // const apiEndpoint = '/api/waiting-list';
+  const apiEndpoint = 'https://satoshiterminal.io/api/waiting-list';
 
   fetch(apiEndpoint, {
     method: 'POST',
