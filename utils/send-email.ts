c2093025cf261cde168402d49f725a29d3d413cc
danmiles <41701228+danmiles@users.noreply.github.com>
2023-10-16 16:33:27 +0200

    commit

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -2,7 +2,7 @@
 import { WaitingListFormData } from '@/components/WaitingListForm';
 
 export function sendEmail(data: WaitingListFormData) {
-  const apiEndpoint = '/api/waitinglist';
+  const apiEndpoint = '/api/waiting-list';
 
   fetch(apiEndpoint, {
     method: 'POST',
