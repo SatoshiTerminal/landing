56258f821945ba13a1a528bb24fcaf1b5d93eab4
danmiles <41701228+danmiles@users.noreply.github.com>
2023-10-25 21:28:39 +0200

    not worcking 405 now allowed

--- app/api/recaptcha/route.ts
+++ app/api/recaptcha/route.ts
@@ -1,23 +1,36 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
-const NEXT_PUBLIC_CAPTCHA_SECRET_KEY = process.env.NEXT_PUBLIC_CAPTCHA_SECRET_KEY;
+const NEXT_PUBLIC_CAPTCHA_SECRET_KEY =
+  process.env.NEXT_PUBLIC_CAPTCHA_SECRET_KEY;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { token } = req.body;
+export default async function handler(req: NextRequest) {
+  const { token }: any = req.body;
 
   try {
     const response = await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${NEXT_PUBLIC_CAPTCHA_SECRET_KEY}&response=${token}`
+      `https://www.google.com/recaptcha/api/siteverify?secret=${NEXT_PUBLIC_CAPTCHA_SECRET_KEY}&response=${token}`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
     );
 
     if (response.data.success) {
-      res.status(200).json({ message: 'Success' });
+      NextResponse.json({
+        success: true,
+      });
     } else {
-      res.status(400).json({ message: 'Failed' });
+      NextResponse.json(
+        { error: 'Captcha verification failed' },
+        { status: 400 }
+      );
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
 }
