58ab7a81fe91d33b4039fa5de5cc0eea3f54f115
codephilic <kwl.c0d3r@gmail.com>
2023-10-28 22:52:28 +0800

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,52 @@
-// import { Analytics } from '@vercel/analytics/react';
-import './globals.css';
-import 'animate.css';
-import type { Metadata } from 'next';
-import { Outfit, Plus_Jakarta_Sans  } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
+import "animate.css";
+import type { Metadata } from "next";
+import { Outfit, Plus_Jakarta_Sans } from "next/font/google";
 
 // ==== Components ====
 // Navbar srool to sections uncomment if needed
 // import Navbar from '@/components/navbar-scroll-section/NavbarScrollTo';
 // Footer
-import Footer from '@/components/Footer';
+import Footer from "@/components/Footer";
 
 // Fonts here
 const outfit = Outfit({
-  weight: ['400', '500', '600',],
-  style: ['normal'],
-  subsets: ['latin'],
-  variable: '--font-outfit',
-  display: 'swap',
+  weight: ["400", "500", "600"],
+  style: ["normal"],
+  subsets: ["latin"],
+  variable: "--font-outfit",
+  display: "swap",
 });
 
 const plus_jakarta_sans = Plus_Jakarta_Sans({
-  weight: ['500', '600', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  variable: '--plus-jakarta-sans',
-  display: 'swap',
+  weight: ["500", "600", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  variable: "--plus-jakarta-sans",
+  display: "swap",
 });
 
 // Metadata for SEO
 
 export const metadata: Metadata = {
-  title: 'Satoshi Terminal',
+  title: "Satoshi Terminal",
   description:
-    'The Bloomberg Terminal for Crypto. Allowing Wall St to research crypto the same way they research stocks and bonds. The world’s first crypto research platform for Wall Street, designed & developed by former Bloomberg Terminal engineers.',
+    "The Bloomberg Terminal for Crypto. Allowing Wall St to research crypto the same way they research stocks and bonds. The world’s first crypto research platform for Wall Street, designed & developed by former Bloomberg Terminal engineers.",
   openGraph: {
-    title: 'Satoshi Terminal',
+    title: "Satoshi Terminal",
     description:
-      'The Bloomberg Terminal for Crypto. Allowing Wall St to research crypto the same way they research stocks and bonds. The world’s first crypto research platform for Wall Street, designed & developed by former Bloomberg Terminal engineers.',
+      "The Bloomberg Terminal for Crypto. Allowing Wall St to research crypto the same way they research stocks and bonds. The world’s first crypto research platform for Wall Street, designed & developed by former Bloomberg Terminal engineers.",
     images: [
       {
-        url: '/images/home-meta__img.jpg',
+        url: "/images/home-meta__img.jpg",
         width: 1280,
         height: 720,
       },
     ],
   },
   alternates: {
-    canonical: 'https://satoshiterminal.io',
+    canonical: "https://satoshiterminal.io",
   },
 };
 
@@ -57,12 +57,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${outfit.variable} ${plus_jakarta_sans.variable}`}>        
+      <body className={`${outfit.variable} ${plus_jakarta_sans.variable}`}>
         {/* <Navbar /> */}
         {children}
         <Footer />
         {/* Vercel Analytics */}
-        {/* <Analytics /> */}
+        <Analytics />
       </body>
     </html>
   );
